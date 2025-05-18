a5c5d1f5404e63b8162b66ec23a777ea831719de
Mahesha V P <maheshavp12@gmail.com>
2025-05-19 00:28:53 +0530

    Fruit-store

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Menus from './components/Menus/Menus'
+import Banner from './components/Banners/Banner'
+import Banner2 from './components/Banners/Banner2'
+import Banner3 from './components/Banners/Banner3'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
@@ -10,6 +14,10 @@ const App = () => {
         <Navbar />
         <Hero />
         <Menus />  
+        <Banner />
+        <Banner2 />
+        <Banner3 />
+        <Footer />
       </main>
     </>
   )
--- src/components/Menus/Menus.jsx
+++ src/components/Menus/Menus.jsx
@@ -3,7 +3,8 @@ import Fruit1 from "../../assets/fruits/apple.png"
 import Fruit2 from "../../assets/fruits/orange.png"
 import Fruit3 from "../../assets/fruits/avocado.png"
 import Fruit4 from "../../assets/fruits/cherry.png"
-import { motion } from 'framer-motion'
+import {  motion } from 'framer-motion'
+import { FadeLeft } from '../../utility/animation'
 
 const MenuData =  [
     {
@@ -12,6 +13,7 @@ const MenuData =  [
         link: '/',
         price: "RS.200",
         image: Fruit1,
+        delay: 0.3,
     },
     {
         id: 2,
@@ -19,6 +21,7 @@ const MenuData =  [
         link: '/',
         price: "RS.150",
         image: Fruit2,
+        delay: 0.6,
     },
     {
         id: 3,
@@ -26,6 +29,7 @@ const MenuData =  [
         link: '/',
         price: "RS.120",
         image: Fruit3,
+        delay: 0.9,
     },
     {
         id: 4,
@@ -33,6 +37,7 @@ const MenuData =  [
         link: '/',
         price: "RS.400",
         image: Fruit4,
+        delay: 1.2,
     },
 ];
 
@@ -40,17 +45,26 @@ const Menus = () => {
   return (
     <section>
         <div className="container pt-12 pb-20">
-            <h1 className='text-2xl font-bold text-left pb-10 uppercase'>
-                Our Menu</h1>
+            <motion.h1 
+            initial={{opacity: 0, x: -200 }}
+            whileInView={{opacity: 1, x: 0}}
+            transition={{duration: 1, delay: 0.3}}
+            className='text-2xl font-bold text-left pb-10 uppercase'>
+                Our Menu</motion.h1>
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8'>
                 {MenuData.map((menu) => (
-                    <div className='bg-white rounded-3xl px-4 py-2 shadow-[0_0_22px_0_rgba(0,0,0,0.15)] flex flex-row justify-around items-center gap-3'>
+                    <motion.div 
+                    variants={FadeLeft(menu.delay)}
+                    initial = "hidden"
+                    whileInView={"visible"}
+                    whileHover={{scale: 1.1}}
+                    className='bg-white rounded-3xl px-4 py-2 shadow-[0_0_22px_0_rgba(0,0,0,0.15)] flex flex-row justify-around items-center gap-3'>
                         <img src={menu.image} alt="img" className='w-[60px] mb-4 scale-110  transform -translate-y-6'/>
                         <div className=''>
                             <h1 className='text-lg font-semibold'>{menu.title}</h1>
                             <p className='text-lg font-semibold text-secondary'>{menu.price}</p>
                         </div>
-                    </div>
+                    </motion.div>
                 ))}
             </div>
         </div>
